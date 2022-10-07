9355174fc16f147ddaaa6b582aeb0d13fa910a67
Tudor Frateanu 2E2 <79188237+FrateanuTudor1E2@users.noreply.github.com>
2022-10-07 13:34:52 +0300

    -

--- lab2pmp/EX1.py
+++ lab2pmp/EX1.py
@@ -7,8 +7,7 @@ import arviz as az
 omega_1 = stats.expon.rvs(0, 1 / 4, size=10000)
 omega_2 = stats.expon.rvs(0, 1 / 6, size=10000)
 
-M = (omega_1 + omega_2)/2
+M = (omega_1 + omega_2) / 2
 
-az.plot_posterior({'Timp mecanic 1':omega_1,'Timp mecanic 2':omega_2,'Media':M})
+az.plot_posterior({'Timp mecanic 1': omega_1, 'Timp mecanic 2': omega_2, 'Media': M})
 plt.show()
-

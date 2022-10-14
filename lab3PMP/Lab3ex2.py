4718202d76dac0548b8073d5283b206853e7c1d8
Tudor Frateanu 2E2 <79188237+FrateanuTudor1E2@users.noreply.github.com>
2022-10-14 13:47:03 +0300

    lab3pmp

--- lab3PMP/Lab3ex2.py
+++ lab3PMP/Lab3ex2.py
@@ -13,7 +13,7 @@ with model:
     cutremur = pm.Bernoulli('C', 0.0005)
     incendiu_p = pm.Deterministic('I_p', pm.math.switch(cutremur, 0.03, 0.01))
     incendiu = pm.Bernoulli('I', p=incendiu_p)
-    alarma_p = pm.Deterministic((A_p), pm.math.switch(cutremur, pm.math.switch(incendiu, 0.98, 0.02),pm.math.switch(incendiu,99.9999, 0.0001)))
+    alarma_p = pm.Deterministic('A_p'), pm.math.switch(cutremur, pm.math.switch(incendiu, 0.98, 0.02),pm.math.switch(incendiu,99.9999, 0.0001)))
     alarma = pm.Bernoulli('A', p=alarma_p, obderved=1)
 
     trace = pm.sample(200000)
